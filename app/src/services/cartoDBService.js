3cf90aebf67e822987c16c6cdb54b3ecd5c273d1
rrequero <rrequero@gmail.com>
2017-05-29 15:53:34 +0200

    add areaha calculate

--- app/src/services/cartoDBService.js
+++ app/src/services/cartoDBService.js
@@ -1,49 +1,46 @@
 'use strict';
-var logger = require('logger');
-var path = require('path');
-var config = require('config');
-var CartoDB = require('cartodb');
-var Mustache = require('mustache');
-var NotFound = require('errors/notFound');
-var JSONAPIDeserializer = require('jsonapi-serializer').Deserializer;
+const logger = require('logger');
+const path = require('path');
+const config = require('config');
+const CartoDB = require('cartodb');
+const Mustache = require('mustache');
+const NotFound = require('errors/notFound');
+const GeostoreService = require('services/geostoreService');
 
 const WORLD = `WITH poly AS (SELECT * FROM ST_Transform(ST_SimplifyPreserveTopology(ST_SetSRID(ST_MakeValid(ST_GeomFromGeoJSON('{{{geojson}}}')), 4326), 0.01), 3857) geojson)
-             SELECT data_type,
-               (sum( st_area(st_makevalid(ST_Intersection(poly.geojson, i.the_geom_webmercator)))) / (100 * 100)) AS value
+             SELECT data_type
              FROM imazon_sad i, poly        WHERE i.date >= '{{begin}}'::date
                AND i.date <= '{{end}}'::date  and st_intersects(poly.geojson, i.the_geom_webmercator) group by data_type `;
 
-const AREA = `select ST_Area(ST_SetSRID(ST_GeomFromGeoJSON('{{{geojson}}}'), 4326), TRUE)/1000 as area_ha`;
-
 const ISO = `
-        with p as (SELECT the_geom_webmercator, (ST_Area(geography(the_geom))/10000) as area_ha FROM gadm2_countries_simple WHERE iso = UPPER('{{iso}}'))
+        with p as (SELECT the_geom_webmercator FROM gadm2_countries_simple WHERE iso = UPPER('{{iso}}'))
         SELECT data_type,
-            sum(ST_Area(i.the_geom_webmercator)/(100*100)) AS value,  area_ha
+            sum(ST_Area(i.the_geom_webmercator)/(100*100)) AS value
             {{additionalSelect}}
         FROM imazon_sad i right join p on st_intersects(i.the_geom_webmercator, p.the_geom_webmercator)
         WHERE i.date >= '{{begin}}'::date
             AND i.date <= '{{end}}'::date
-        GROUP BY data_type,  area_ha `;
-
-const ID1 = `with p as (SELECT the_geom_webmercator, (ST_Area(geography(the_geom))/10000) as area_ha FROM gadm2_provinces_simple WHERE iso = UPPER('{{iso}}') AND id_1 = {{id1}})
-        SELECT data_type, SUM(ST_Area( ST_Intersection( i.the_geom_webmercator, p.the_geom_webmercator))/(10000)) AS value, area_ha
+        GROUP BY data_type `;
+const AREA = `select ST_Area(ST_SetSRID(ST_GeomFromGeoJSON('{{{geojson}}}'), 4326), TRUE)/1000 as area_ha`;
+const ID1 = `with p as (SELECT the_geom_webmercator FROM gadm2_provinces_simple WHERE iso = UPPER('{{iso}}') AND id_1 = {{id1}})
+        SELECT data_type, SUM(ST_Area( ST_Intersection( i.the_geom_webmercator, p.the_geom_webmercator))/(10000)) AS value
         FROM imazon_sad i right join p on st_intersects(i.the_geom_webmercator, p.the_geom_webmercator)
         and i.date >= '{{begin}}'::date
         AND i.date <= '{{end}}'::date
-        GROUP BY data_type, area_ha`;
+        GROUP BY data_type`;
 
 const USE = `SELECT data_type, SUM(ST_Area(ST_Intersection(
                 i.the_geom_webmercator,
-                p.the_geom_webmercator))/(10000)) AS value, area_ha
+                p.the_geom_webmercator))/(10000)) AS value
                {{additionalSelect}}
-        FROM {{useTable}} p left join imazon_sad i
+        FROM {{useTable}} p inner join imazon_sad i
             on ST_Intersects(
                 i.the_geom_webmercator,
                 p.the_geom_webmercator)
             AND i.date >= '{{begin}}'::date
             AND i.date <= '{{end}}'::date
             where p.cartodb_id = {{pid}}
-        GROUP BY data_type, area_ha `;
+        GROUP BY data_type`;
 
 const WDPA = `WITH  p as ( SELECT
             CASE WHEN marine::numeric = 2
@@ -56,15 +53,15 @@ const WDPA = `WITH  p as ( SELECT
             END as the_geom_webmercator, gis_area*100 as area_ha FROM wdpa_protected_areas where wdpaid={{wdpaid}})
             SELECT data_type, SUM(ST_Area(ST_Intersection(
                             i.the_geom_webmercator,
-                            p.the_geom_webmercator))/(10000)) AS value, area_ha
+                            p.the_geom_webmercator))/(10000)) AS value
             {{additionalSelect}}
             FROM p
-            left join imazon_sad i
+            inner join imazon_sad i
             on st_intersects(i.the_geom_webmercator,
                             p.the_geom_webmercator)
             and i.date >= '{{begin}}'::date
                         AND i.date <= '{{end}}'::date
-            GROUP BY data_type, area_ha`;
+            GROUP BY data_type`;
 
 const LATEST = `SELECT DISTINCT date
         FROM imazon_sad
@@ -84,13 +81,6 @@ var executeThunk = function(client, sql, params) {
         });
     };
 };
-
-var deserializer = function(obj) {
-    return function(callback) {
-        new JSONAPIDeserializer({keyForAttribute: 'camelCase'}).deserialize(obj, callback);
-    };
-};
-
 
 let getToday = function() {
     let today = new Date();
@@ -149,22 +139,28 @@ class CartoDBService {
         if (alertQuery) {
             params.additionalSelect = MIN_MAX_DATE_SQL;
         }
-        if (iso === 'BRA') {
-            let data = yield executeThunk(this.client, ISO, params);
-            if (data.rows) {
-                let result = {
-                    value: data.rows
-                };
-                if(data.rows.length > 0){
-                    result.area_ha = data.rows[0].area_ha;
+        let geostore = yield GeostoreService.getGeostoreByIso(iso);
+        if (geostore) {
+            if (iso === 'BRA') {
+                let data = yield executeThunk(this.client, ISO, params);
+                if (data.rows) {
+                    let result = {
+                        value: data.rows
+                    };
+                    result.area_ha = geostore.areaHa;
+                    result.downloadUrls = this.getDownloadUrls(ISO, params);
+                    return result;
+                } else {
+                    return {
+                        area_ha: geostore.areaHa   
+                    };
                 }
-                result.downloadUrls = this.getDownloadUrls(ISO, params);
-                return result;
+            } else {
+                return {
+                    value: [],
+                    area_ha: geostore.areaHa
+                };
             }
-        } else {
-            return {
-                value: []
-            };
         }
         return null;
     }
@@ -182,28 +178,35 @@ class CartoDBService {
         if (alertQuery) {
             params.additionalSelect = MIN_MAX_DATE_SQL;
         }
-        if (iso === 'BRA') {
-            let data = yield executeThunk(this.client, ID1, params);
-            if (data.rows) {
-                let result = {
-                    value: data.rows
-                };
-                if(data.rows.length > 0){
-                    result.area_ha = data.rows[0].area_ha;
+        let geostore = yield GeostoreService.getGeostoreByIsoAndId(iso, id1);
+        if (geostore){ 
+            if (iso === 'BRA') {
+                let data = yield executeThunk(this.client, ID1, params);
+                if (data.rows) {
+                    let result = {
+                        value: data.rows
+                    };
+                    result.area_ha = geostore.areaHa;
+                    result.downloadUrls = this.getDownloadUrls(ID1, params);
+                    return result;
+                } else {
+                    return {
+                        value: [],
+                        area_ha: geostore.areaHa
+                    };
                 }
-                result.downloadUrls = this.getDownloadUrls(ISO, params);
-                return result;
+            } else {
+                return {
+                    value: [],
+                    area_ha: geostore.areaHa
+                };
             }
-        } else {
-            return {
-                value: []
-            };
         }
         return null;
     }
 
     *
-    getUse(useTable, id, alertQuery, period = defaultDate()) {
+    getUse(useName, useTable, id, alertQuery, period = defaultDate()) {
         logger.debug('Obtaining use with id %s', id);
         let periods = period.split(',');
         let params = {
@@ -215,17 +218,23 @@ class CartoDBService {
         if (alertQuery) {
             params.additionalSelect = MIN_MAX_DATE_SQL;
         }
-        let data = yield executeThunk(this.client, USE, params);
 
-        if (data.rows) {
-            let result = {
-                value: data.rows
-            };
-            if(data.rows.length > 0){
-                result.area_ha = data.rows[0].area_ha;
+        const geostore = yield GeostoreService.getGeostoreByUse(useName, id);
+        let data = yield executeThunk(this.client, USE, params);
+        if (geostore){
+            if (data.rows && data.rows.length > 0 && data.rows[0].data_type !== null) {
+                let result = {
+                    value: data.rows
+                };
+                result.area_ha = geostore.areaHa;
+                result.downloadUrls = this.getDownloadUrls(USE, params);
+                return result;
+            } else {
+                return {
+                    value: [],
+                    area_ha: geostore.areaHa
+                };
             }
-            result.downloadUrls = this.getDownloadUrls(ISO, params);
-            return result;
         }
         return null;
     }
@@ -242,41 +251,31 @@ class CartoDBService {
         if (alertQuery) {
             params.additionalSelect = MIN_MAX_DATE_SQL;
         }
+        const geostore = yield GeostoreService.getGeostoreByWdpa(wdpaid);
         let data = yield executeThunk(this.client, WDPA, params);
-        if (data.rows) {
-            let result = {
-                value: data.rows
-            };
-            if(data.rows.length > 0){
-                result.area_ha = data.rows[0].area_ha;
+        if (geostore ) {
+            if (data.rows) {
+                let result = {
+                    value: data.rows
+                };
+                result.area_ha = geostore.areaHa;
+                result.downloadUrls = this.getDownloadUrls(WDPA, params);
+                return result;
+            } else {
+                return {
+                    value: [],
+                    area_ha: geostore.areaHa
+                };
             }
-            result.downloadUrls = this.getDownloadUrls(ISO, params);
-            return result;
         }
         return null;
-    }
-
-    *
-    getGeostore(hashGeoStore) {
-        logger.debug('Obtaining geostore with hash %s', hashGeoStore);
-        let result = yield require('vizz.microservice-client').requestToMicroservice({
-            uri: '/geostore/' + hashGeoStore,
-            method: 'GET',
-            json: true
-        });
-        if (result.statusCode !== 200) {
-            console.error('Error obtaining geostore:');
-            console.error(result);
-            return null;
-        }
-        return yield deserializer(result.body);
     }
 
     *
     getWorld(hashGeoStore, alertQuery, period = defaultDate()) {
         logger.debug('Obtaining world with hashGeoStore %s', hashGeoStore);
 
-        let geostore = yield this.getGeostore(hashGeoStore);
+        const geostore = yield GeostoreService.getGeostoreByHash(hashGeoStore);
         if (geostore && geostore.geojson) {
             logger.debug('Executing query in cartodb with geojson', geostore.geojson);
             let periods = period.split(',');
@@ -293,9 +292,9 @@ class CartoDBService {
                 let result = {
                     value: data.rows
                 };
-                result.area_ha = geostore.area_ha;
+                result.area_ha = geostore.areaHa;
                 
-                result.downloadUrls = this.getDownloadUrls(ISO, params);
+                result.downloadUrls = this.getDownloadUrls(WORLD, params);
                 return result;
             }
             return null;
