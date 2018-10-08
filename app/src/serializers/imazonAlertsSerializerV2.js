0af7562bd319cf7aeb4e47a81941c326a4d6c8db
Adam J. Pain <aj.pain89@gmail.com>
2018-10-08 12:15:55 +0200

    Adds v2

---
+++ app/src/serializers/imazonAlertsSerializerV2.js
@@ -0,0 +1,37 @@
+
+'use strict';
+
+var logger = require('logger');
+var JSONAPISerializer = require('jsonapi-serializer').Serializer;
+var imazonAlertsSerializerV2 = new JSONAPISerializer('imazon-alerts', {
+    attributes: ['value','downloadUrls', 'area_ha'],
+    typeForAttribute: function (attribute, record) {
+        return attribute;
+    },
+    downloadUrls:{
+        attributes: ['csv', 'geojson', 'kml', 'shp', 'svg']
+    },
+    value: {
+        attributes: ['label', 'value', 'unit', 'min_date', 'max_date']
+    },
+    keyForAttribute: 'camelCase'
+});
+var imazonLatestSerializer = new JSONAPISerializer('imazon-latest', {
+    attributes: ['latest'],
+    typeForAttribute: function (attribute, record) {
+        return attribute;
+    }
+});
+
+class ImazonAlertsSerializerV2 {
+
+  static serialize(data) {
+    return imazonAlertsSerializerV2.serialize(data);
+  }
+
+  static serializeLatest(data) {
+    return imazonLatestSerializer.serialize(data);
+  }
+}
+
+module.exports = ImazonAlertsSerializerV2;
